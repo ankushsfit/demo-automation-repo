686d5eac378dfc26f8abf780d7365bee4c42e484
ankushsfit <ankush@simplefixit.com>
2026-07-27 18:36:48 +0530

    Added allure on git hub pages

--- src/test/java/tests/TodoTest.java
+++ src/test/java/tests/TodoTest.java
@@ -1,39 +1,35 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.TodoPage;
 
-public class TodoTest extends BaseTest{
-		
-	
-	@Test(priority=1)
+public class TodoTest extends BaseTest {
+
+	@Test(priority = 1)
 	public void verifyTodoTaskCreation() {
 		TodoPage todoPage = new TodoPage(driver);
-		todoPage.todoTaskCreation("Need to perform ci/cd");		
-		todoPage.todoTaskCreation("Need to practice on github actions");		
+		todoPage.todoTaskCreation("Need to perform ci/cd");
+		todoPage.todoTaskCreation("Need to practice on github actions");
 	}
-	
-	
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void verifyDashboardText() {
 		TodoPage todoPage = new TodoPage(driver);
 		String dashText = todoPage.isDashboardTextCorrect();
 		System.out.println("Dashboard Text: " + dashText);
 		Assert.assertEquals(dashText, "TODO");
 	}
-	
-	@Test(priority=3)
-	
+
+	@Test(priority = 3)
+
 	public void verifyTotalTaskCount() {
 		TodoPage todoPage = new TodoPage(driver);
 		int totalTasks = todoPage.totalTaskCount();
-		System.out.println("Total: "+totalTasks);
+		System.out.println("Total Tasks: " + totalTasks);
 		Assert.assertEquals(totalTasks, 2);
 	}
-
 
 }

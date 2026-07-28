50e0b39de43cb618d9f099274a004bea36a378dc
ankushsfit <ankush@simplefixit.com>
2026-07-28 13:22:24 +0530

    pipeline testing

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,7 +1,10 @@
 package listeners;
 
+import java.io.File;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.IExecutionListener;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -9,7 +12,31 @@ import drivers.DriverFactory;
 import io.qameta.allure.Attachment;
 import utils.ScreenshotUtil;
 
-public class TestListener implements ITestListener {
+public class TestListener implements ITestListener, IExecutionListener {
+
+	@Override
+	public void onExecutionStart() {
+
+		System.out.println("Cleaning old Allure results...");
+
+		File allureResults = new File("allure-results");
+
+		if (allureResults.exists()) {
+
+			deleteFolder(allureResults);
+
+			System.out.println("Old Allure results deleted");
+
+		}
+
+	}
+
+	@Override
+	public void onExecutionFinish() {
+
+		System.out.println("Test execution finished");
+
+	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
@@ -26,6 +53,32 @@ public class TestListener implements ITestListener {
 	public byte[] attachScreenshotToAllure() {
 
 		return ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+	}
+
+	private void deleteFolder(File folder) {
+
+		File[] files = folder.listFiles();
+
+		if (files != null) {
+
+			for (File file : files) {
+
+				if (file.isDirectory()) {
+
+					deleteFolder(file);
+
+				} else {
+
+					file.delete();
+
+				}
+
+			}
+
+		}
+
+		folder.delete();
 
 	}
 
